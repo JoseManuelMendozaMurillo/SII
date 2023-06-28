cdaa88b5973c5c8786a739fe8dfb02906fe37723
Miguel Angel Salazar Martinez <128423820+SalazarMtzMiguel@users.noreply.github.com>
2023-06-28 17:56:09 -0600

    ADD scripts Generacion tablas y relaciones

---
+++ app/Database/new_insercion.sql
@@ -0,0 +1,11 @@
+/*Insertar valores en tabla carreras*/
+insert into carreras
+values
+(0,'Ingenieria en Sistemas Computacionales','ISCTEC','ISCTECNM','ISC',200,35,45),
+(0,'Ingenieria en Logistica','ILTEC','ILTECNM','IL',200,15,45),
+(0,'Ingenieria en Electromecanica','IETEC','IETECNM','IE',200,35,65),
+(0,'Ingenieria en Gestion Empresarial','IGETEC','IGETECNM','IGE',200,35,45),
+(0,'Ingenieria Industrial','IITEC','IITECNM','II',200,35,85),
+(0,'Licenciatura en Contabilidad','LCTEC','LCTECNM','LC',200,25,55);
+/**/
+insert into 
