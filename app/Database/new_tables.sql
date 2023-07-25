9243eec7d5b645d47d816a977455ec8ddc76b006
Miguel Angel Salazar Martinez <128423820+SalazarMtzMiguel@users.noreply.github.com>
2023-07-24 20:29:48 -0600

    Antes de que muera mi lap

--- app/Database/new_tables.sql
+++ app/Database/new_tables.sql
@@ -3,6 +3,131 @@ drop database control_escolar;
 CREATE DATABASE IF NOT EXISTS control_escolar;
 
 use control_escolar;
+/*tablas de permisos*/
+
+CREATE TABLE `users` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(30) DEFAULT NULL,
+    `status` varchar(255) DEFAULT NULL,
+    `status_message` varchar(255) DEFAULT NULL,
+    `active` tinyint(1) NOT NULL DEFAULT '0',
+    `last_active` datetime DEFAULT NULL,
+    `created_at` datetime DEFAULT NULL,
+    `updated_at` datetime DEFAULT NULL,
+    `deleted_at` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+
+
+
+CREATE TABLE `auth_groups_users` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `user_id` int unsigned NOT NULL,
+    `group` varchar(255) NOT NULL,
+    `created_at` datetime NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `auth_groups_users_user_id_foreign` (`user_id`),
+    CONSTRAINT `auth_groups_users_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `auth_identities` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `user_id` int unsigned NOT NULL,
+    `type` varchar(255) NOT NULL,
+    `name` varchar(255) DEFAULT NULL,
+    `secret` varchar(255) NOT NULL,
+    `secret2` varchar(255) DEFAULT NULL,
+    `expires` datetime DEFAULT NULL,
+    `extra` text,
+    `force_reset` tinyint(1) NOT NULL DEFAULT '0',
+    `last_used_at` datetime DEFAULT NULL,
+    `created_at` datetime DEFAULT NULL,
+    `updated_at` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `type_secret` (`type`,`secret`),
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `auth_identities_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `auth_logins` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `ip_address` varchar(255) NOT NULL,
+    `user_agent` varchar(255) DEFAULT NULL,
+    `id_type` varchar(255) NOT NULL,
+    `identifier` varchar(255) NOT NULL,
+    `user_id` int unsigned DEFAULT NULL,
+    `date` datetime NOT NULL,
+    `success` tinyint(1) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `id_type_identifier` (`id_type`,`identifier`),
+    KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `auth_permissions_users` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `user_id` int unsigned NOT NULL,
+    `permission` varchar(255) NOT NULL,
+    `created_at` datetime NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `auth_permissions_users_user_id_foreign` (`user_id`),
+    CONSTRAINT `auth_permissions_users_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+CREATE TABLE `auth_remember_tokens` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `selector` varchar(255) NOT NULL,
+    `hashedValidator` varchar(255) NOT NULL,
+    `user_id` int unsigned NOT NULL,
+    `expires` datetime NOT NULL,
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `selector` (`selector`),
+    KEY `auth_remember_tokens_user_id_foreign` (`user_id`),
+    CONSTRAINT `auth_remember_tokens_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+CREATE TABLE `auth_token_logins` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `ip_address` varchar(255) NOT NULL,
+    `user_agent` varchar(255) DEFAULT NULL,
+    `id_type` varchar(255) NOT NULL,
+    `identifier` varchar(255) NOT NULL,
+    `user_id` int unsigned DEFAULT NULL,
+    `date` datetime NOT NULL,
+    `success` tinyint(1) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `id_type_identifier` (`id_type`,`identifier`),
+    KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `migrations` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `version` varchar(255) NOT NULL,
+    `class` varchar(255) NOT NULL,
+    `group` varchar(255) NOT NULL,
+    `namespace` varchar(255) NOT NULL,
+    `time` int NOT NULL,
+    `batch` int unsigned NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `settings` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `class` varchar(255) NOT NULL,
+    `key` varchar(255) NOT NULL,
+    `value` text,
+    `type` varchar(31) NOT NULL DEFAULT 'string',
+    `context` varchar(255) DEFAULT NULL,
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+/*termina bloque*/
 
 Create TABLE constantes(
     carga_minima int,
