3d2dc7051d2118d45ed7db496a58e4086b80b055
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2023-08-02 15:48:35 +0000

    2023-08-02 - Se le dio formato al archivo validate.php

--- app/Controllers/Test/Validate/validate.php
+++ app/Controllers/Test/Validate/validate.php
@@ -4,8 +4,9 @@ namespace App\Controllers\Test\Validate;
 
 use App\Controllers\Test\Validate\CustomValidations;
 
-function validateLogin(){
-    $validation = service ('validation');
+function validateLogin()
+{
+    $validation = service('validation');
 
     $validation->setRules([
         'email' => 'required|valid_email|CustomValidations.ValidateEmail',
@@ -28,6 +29,7 @@ function validateLogin(){
         if (password_verify($password, $user['password'])) {// password_verify si estamos usando el metodo password_hash
             // La contraseña es correcta, inicia la sesión
             session()->set('user', $user);
+
             return redirect()->to('/Inicio xd'); // Dirigir a la pagina de inicio o mandar un booleano
         } else {
             // La contraseña es incorrecta
@@ -39,49 +41,50 @@ function validateLogin(){
     }
 }
 
-function validateSingup(){
-    $validation = service ('validation');
+function validateSingup()
+{
+    $validation = service('validation');
 
-    $validation -> setRules([
+    $validation->setRules([
         //Informacion de la cuenta
-        'email'=> [
+        'email' => [
             'rules' => 'required|valid_email|is_unique[alumnos.email]',
             'errors' => [
                 'required' => 'El campo correo electrónico es obligatorio',
                 'valid_email' => 'Debe proporcionar un correo electrónico válido',
                 'is_unique' => 'El correo electrónico ya está en uso',
-            ]
+            ],
         ],
         'confirmEmail' => 'required|matches[email]',
-        'password' =>'required|min_length[8]',
+        'password' => 'required|min_length[8]',
         'confirmPassword' => 'required|matches[password]',
 
         //Informacion personal
-        'curp'   => [
+        'curp' => [
             'rules' => 'required|alpha_numeric|is_unique[alumnos.curp]',
             'errors' => [
                 'required' => 'El campo CURP es obligatorio',
                 'is_unique' => 'Esta CURP ya está en uso',
-            ]
+            ],
         ],
-        'nameInfo' =>'required|alpha_space',
-        'surnamePaterno' =>'required|alpha_space',
-        'surnameMaterno' =>'required|alpha_space',
-        'birthDate'  =>'required',
-        'gender'  =>'required',
-        'maritalStatus'  =>'required',
-        'birthContry'  =>'required',
-        'phone1' =>'required|numeric',
-        'phone1' =>'permit_empty|numeric',
+        'nameInfo' => 'required|alpha_space',
+        'surnamePaterno' => 'required|alpha_space',
+        'surnameMaterno' => 'required|alpha_space',
+        'birthDate' => 'required',
+        'gender' => 'required',
+        'maritalStatus' => 'required',
+        'birthContry' => 'required',
+        'phone1' => 'required|numeric',
+        'phone1' => 'permit_empty|numeric',
 
         //Informacion del domicilio
-        'street'=>'required',
-        'suburb'=>'required',
-        'outN'=>'required|numeric',
-        'intN'=>'permit_empty|numeric',
-        'zipcode' =>'required|numeric',
-        'city'  =>'required',
-        'state'   =>'required',
-        'betweenStreets' => 'required'
+        'street' => 'required',
+        'suburb' => 'required',
+        'outN' => 'required|numeric',
+        'intN' => 'permit_empty|numeric',
+        'zipcode' => 'required|numeric',
+        'city' => 'required',
+        'state' => 'required',
+        'betweenStreets' => 'required',
     ]);
 }
