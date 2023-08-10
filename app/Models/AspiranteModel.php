257e1a44d2a47225d3bc8a815384a2d997c730ef
Oscar Dev <oscargs.dev@gmail.com>
2023-08-10 22:15:10 +0000

    SII-37 Crear un modelo para interactuar con la tabla de aspirantes

--- app/Models/AspiranteModel.php
+++ app/Models/AspiranteModel.php
@@ -2,16 +2,67 @@
 
 namespace App\Models;
 
+use App\Entities\Aspirante;
 use CodeIgniter\Model;
 
 class AspiranteModel extends Model
 {
-    // TODO: Cambiar el nombre de la tabla si es que se requiere
     protected $table = 'aspirantes';
     protected $allowedFields = [
-        // TODO: Asignar los campos permitidos
-        // 'username', 'email', 'password',
+        'user_id',
+        'imagen',
+        'curp',
+        'apellido_paterno',
+        'apellido_materno',
+        'nombre',
+        'fecha_nacimiento',
+        'genero',
+        'estado_civil',
+        'pais_nacimiento',
+        'telefono',
+        'email',
+        'escuela_procedencia',
+        'estado',
+        'municipio',
+        'egreso',
+        'promedio_general',
+        'carrera_primera_opcion',
+        'carrera_segunda_opcion',
+        'turno_preferente',
+        'ito_primera_opcion',
+        'motivo_ingreso',
+        'motivo_seleccion_plan_estudios',
     ];
-    protected $returnType = \App\Entities\Aspirante::class;
+
+    // Dates
     protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+    protected $returnType = \App\Entities\Aspirante::class;
+
+    /**
+    * Locates an Aspirante object by ID.
+    *
+     * @param int|string $id
+    */
+    public function findById($id): ?Aspirante
+    {
+        return $this->find($id);
+    }
+
+    // TODO: Implementar queries necesarias, segun sea el requisito
+    public function findByFullName($nombre, $apellido_paterno, $apellido_materno)
+    {
+        // $sql = 'select * from ' . $table . ' where nombre = :nombre: '
+        //         . 'and apellido_paterno = :apellido_paterno: '
+        //         . 'and apellido_materno = :apellido_materno:';
+
+        // return $this->find->
+    }
+
+    public function findByCURP($curp)
+    {
+    }
 }
