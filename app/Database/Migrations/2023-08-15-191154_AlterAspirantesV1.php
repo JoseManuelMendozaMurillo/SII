91885745762da9ddab574e905631d55927866401
root <mikecostilla2000.masm@gmail.com>
2023-08-15 19:48:14 +0000

    SII-88 Nueva estructura Aspirantes y OTRAS

---
+++ app/Database/Migrations/2023-08-15-191154_AlterAspirantesV1.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AlterAspirantesV1 extends Migration
+{
+    public function up()
+    {
+        $sqlFilePath = __DIR__ . '/sql_migrations/alter_aspirantes_2023-08-15.sql';
+        $sqlFileContent = file_get_contents($sqlFilePath);
+        $this->db->query($sqlFileContent);
+    }
+
+    public function down()
+    {
+        $sqlFilePath = __DIR__ . '/sql_rollback/alter_aspirantes_2023-08-15.sql';
+        $sqlFileContent = file_get_contents($sqlFilePath);
+        $this->db->query($sqlFileContent);
+    }
+}
---
+++ app/Database/Migrations/sql_rollback/alter_aspirantes_2023-08-15.sql
@@ -0,0 +1,4 @@
+ALTER TABLE aspirantes 
+drop column no_solicitud,
+drop column nip,
+drop column estatus_pago;
