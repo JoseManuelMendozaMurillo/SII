0ee5ad2233899258df66b31a81ec60cc58ac3936
mike <mikecostilla2000.masm@gmail.com>
2023-08-15 12:15:42 -0600

    cambios de permisos

--- app/Database/Seeds/LlenadoInfoBancaria.php
+++ app/Database/Seeds/LlenadoInfoBancaria.php
@@ -8,6 +8,7 @@ class LlenadoInfoBancaria extends Seeder
 {
     public function run()
     {
+        //No jalo el seeder normal
         $sqlFilePath = __DIR__ . '/ScripInfoBancaria/insert_infobancaria.sql';
         $sqlFileContent = file_get_contents($sqlFilePath);
         $this->db->query($sqlFileContent);
