11cd8c49f55fbe30b6db9a3a016e47f689d0fde9
root <mikecostilla2000.masm@gmail.com>
2023-08-30 17:16:15 +0000

    SII-101 New Seeder para Aspirantes

---
+++ app/Database/Seeds/LimpiaTablasAspirantes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class LimpiaTablasAspirantes extends Seeder
+{
+    public function run()
+    {
+        $this->db->query(file_get_contents(__DIR__ . '/ScriptsLimpiarShieldAspirantes/DesactivaFKC.sql'));
+        $this->db->query(file_get_contents(__DIR__ . '/ScriptsLimpiarShieldAspirantes/TruncateUsers.sql'));
+        $this->db->query(file_get_contents(__DIR__ . '/ScriptsLimpiarShieldAspirantes/TruncateAspirantes.sql'));
+        $this->db->query(file_get_contents(__DIR__ . '/ScriptsLimpiarShieldAspirantes/TruncateAspirantesDatosC.sql'));
+        $this->db->query(file_get_contents(__DIR__ . '/ScriptsLimpiarShieldAspirantes/TruncateAuthIdentities.sql'));
+        $this->db->query(file_get_contents(__DIR__ . '/ScriptsLimpiarShieldAspirantes/DesactivaFKC.sql'));
+    }
+}
