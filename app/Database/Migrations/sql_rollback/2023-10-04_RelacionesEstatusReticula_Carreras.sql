70cee699d16687f549ec6a10b26c0ec4e5fd5af9
mike <mikecostilla2000.masm@gmail.com>
2023-10-04 19:31:28 +0000

    SII-142 Con datos y todo

---
+++ app/Database/Migrations/sql_rollback/2023-10-04_RelacionesEstatusReticula_Carreras.sql
@@ -0,0 +1,4 @@
+ALTER TABLE
+    carreras DROP FOREIGN KEY fk_carr_est_estret_id_esta,
+    DROP KEY fk_carr_est_estret_id_esta,
+    DROP COLUMN estatus;
---
+++ app/Database/Seeds/ScriptsReticulas/insert_estatusret.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    estatus_reticula (id_estatus, estatus)
+VALUES
+    (0, 'Borrador'),
+    (0, 'Activo'),
+    (0, 'Inactivo'),
+    (0, 'Historial');
