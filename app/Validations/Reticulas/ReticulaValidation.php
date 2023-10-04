d12c1674529f4c2bbaab9ef02091b23e0161a0b6
Oscar Dev <oscargs.dev@gmail.com>
2023-10-04 12:06:35 -0600

    SII-151 Crear reglas de validacion en backend para Reticulas

--- app/Validations/Reticulas/ReticulaValidation.php
+++ app/Validations/Reticulas/ReticulaValidation.php
@@ -4,5 +4,27 @@ namespace App\Validations\Reticulas;
 
 class ReticulaValidation
 {
-    public array $rules = [];
+    public array $rules = [
+        'nombre_reticula' => [
+            'label' => 'Nombre reticula',
+            'rules' => 'required',
+            'errors' => [
+                'required' => 'es obligatorio',
+            ],
+        ],
+        'id_carrera' => [
+            'label' => 'ID carrera',
+            'rules' => 'required',
+            'errors' => [
+                'required' => 'es obligatorio',
+            ],
+        ],
+        'id_especialidad' => [
+            'label' => 'ID especialidad',
+            'rules' => 'required',
+            'errors' => [
+                'required' => 'es obligatorio',
+            ],
+        ],
+    ];
 }
