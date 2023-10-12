817c1d0b930d17a1a2b726bc7aa5e6e54545e651
Oscar Dev <oscargs.dev@gmail.com>
2023-10-11 19:47:29 -0600

    2023-10-11 Creacion de herramienta para la generacion de numeros de control

--- app/Controllers/Alumnos/Alumnos.php
+++ app/Controllers/Alumnos/Alumnos.php
@@ -3,14 +3,25 @@
 namespace App\Controllers\Alumnos;
 
 use App\Controllers\BaseController;
+use App\Libraries\ControlNumber;
 use Exception;
 
 // Alumnos controller
 
 class Alumnos extends BaseController
 {
+    private $control;
+
+    public function __construct()
+    {
+        $this->control = new ControlNumber();
+    }
+
     public function alumno()
     {
-        return 'Hola alumno';
+        for ($i = 0; $i < 10; $i++) {
+            echo $this->control->next();
+            echo '<br>';
+        }
     }
 }
